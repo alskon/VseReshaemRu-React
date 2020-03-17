5662728589939d001d1327dd58b0fd04d483ef15
Alexey <askononenko@mail.ru>
2020-03-17 16:55:46 +0300

    hide firebase API

--- src/Firebase/Firebase.js
+++ src/Firebase/Firebase.js
@@ -1,13 +1,13 @@
 import * as firebase from 'firebase'
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCepgU3di1G51IevEL51WFhz6luGEs7ens",
-    authDomain: "vsereshaem-8493b.firebaseapp.com",
-    databaseURL: "https://vsereshaem-8493b.firebaseio.com",
-    projectId: "vsereshaem-8493b",
-    storageBucket: "vsereshaem-8493b.appspot.com",
-    messagingSenderId: "858006205103",
-    appId: "1:858006205103:web:81a108fed04e5ca6475a5a"
+    apiKey: "******",
+    authDomain: "******",
+    databaseURL: "******",
+    projectId: "******",
+    storageBucket: "******",
+    messagingSenderId: "******",
+    appId: "******",
   };
 
   firebase.initializeApp(firebaseConfig);
